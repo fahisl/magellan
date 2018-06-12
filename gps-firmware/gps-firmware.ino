007411956fb71f5a01405a5d2c0e5a2bc2d2a2e6
Fahimul Islam <fislam@premiumbeat.com>
2018-06-11 21:29:00 -0400

    Make it readable

--- gps-firmware/gps-firmware.ino
+++ gps-firmware/gps-firmware.ino
@@ -1,100 +1,70 @@
-/* Comment this out to disable prints and save space */
-// Libraries for Blynk and ESP8266
 #define BLYNK_PRINT Serial
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
 #include <SimpleTimer.h>
 #include <SoftwareSerial.h>
 #define ESP8266_BAUD 9600
-#define GPSECHO  false // Set to 'true' if you want to debug and listen to the raw GPS sentences. 
 
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO false
 
+// Blynk connection variables
+int heartbeatValue = 0;
+bool connectedToBlynk = false;
 
-// Variables for Blynk, ESP8266, Blynk Connection and Hearthbeat
-int heartbeat ;
-String readString;
-bool Connected2Blynk = false;
-
-
+// Blynk connection configs
 char auth[] = "a9366cf2c7ca444f965db8bd9ac50a76";
 char ssid[] = "tardis";
 char pass[] = "Blake123";
 
-
-
-
-
-
+// ESP8266 config
 SoftwareSerial EspSerial(2, 3); // RX, TX //ESP8266
-
 ESP8266 wifi(&EspSerial);
 
+SimpleTimer timer;
 
-SimpleTimer timerx;
-
-
-void setup()
-{
-  Serial.println("CYTRON_TOUCH_PA1_UNO");
+void setup() {
   Serial.begin(9600);
+  Serial.println("CYTRON_TOUCH_PA1_UNO");
+  
   EspSerial.begin(ESP8266_BAUD);
 
-
+  // TODO: Delays for what?
   delay(10);
   delay(1000);
 
-
-  pinMode(4, INPUT);      // sets the digital pin 4 as input
+  // TODO: Pin 4 is what? Move to global constant
+  pinMode(4, INPUT);
   Blynk.begin(auth, wifi, ssid, pass);
 
-  timerx.setInterval(5000L, CheckConnection);
-  timerx.setInterval(1000L, Heartbeat);
-//  timerx.setInterval(5000L, GPSFunction);
+  timer.setInterval(5000L, checkConnection);
+  timer.setInterval(1000L, heartbeat);
+
+  // TODO: Add GPS method to timer
+  // timerx.setInterval(5000L, GPSFunction);
+
+  Serial.println("Board setup is complete");
 }
 
+void loop() {
+  Blynk.run();
+  timer.run();
+}
 
-
-
-
-
-
-
-
-
-
-
-void CheckConnection() {
-  Connected2Blynk = Blynk.connected();
-  if (!Connected2Blynk) {
+void checkConnection() {
+  connectedToBlynk = Blynk.connected();
+  if (!connectedToBlynk) {
     Serial.println("Not connected to Blynk server");
   }
   else {
     Serial.println("Connected to Blynk server");
-
   }
 }
 
-
-void Heartbeat()
-{
-  // randNumber1 = random(100);
-  float rev = 2;
- heartbeat =  heartbeat +1;
-  Blynk.virtualWrite(V1, heartbeat);
-  Serial.print("Sending Heart Beath ->");
-  Serial.println(heartbeat);
- 
+void heartbeat() {
+  heartbeatValue++;
+  Blynk.virtualWrite(V1, heartbeatValue);
+  Serial.print("Sending heartbeat: ");
+  Serial.println(heartbeatValue);
 }
-
-
-
-
-void loop()
-{
-  Blynk.run();
-  timerx.run();
- 
-
-}
-
 
